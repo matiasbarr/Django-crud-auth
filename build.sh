76abe83afa754e9552afe4c733cced5c283b050d
matiasbarr <matiasbarriga.medrano@gmail.com>
2023-06-22 12:15:42 -0400

    cualquier cosa

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('medrano', 'admin@example.com', 'admin')"
